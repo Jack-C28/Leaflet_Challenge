764f7265e6d554bc320de7b557747434170bedc3
Jack-C28 <56596537+Jack-C28@users.noreply.github.com>
2020-04-13 13:52:43 -0500

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,110 @@
+// Create map background
+console.log("working");
+
+var apiKey = "pk.eyJ1Ijoiampjb29rMjgiLCJhIjoiY2s3d2pwZGN0MDMxYzNtcHBjNTZsc20wZyJ9.2N4cDJc_2AWTAeOl6Cwk4g";
+
+var graymap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: apiKey
+});
+
+// We create the map object with options.
+var map = L.map("mapid", {
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 3
+});
+
+// Add graymap layer.
+graymap.addTo(map);
+
+// AJAX call to retreive JSON data from USGS.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
+
+  // Create style data for each earthquake
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // Determine color based on magnitude
+  function getColor(magnitude) {
+    switch (true) {
+    case magnitude > 5:
+      return "#ea2c2c";
+    case magnitude > 4:
+      return "#ea822c";
+    case magnitude > 3:
+      return "#ee9c00";
+    case magnitude > 2:
+      return "#eecc00";
+    case magnitude > 1:
+      return "#d4ee00";
+    default:
+      return "#98ee00";
+    }
+  }
+
+  // Determine radius of marker based on magnitude
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+    return magnitude * 4;
+  }
+
+  // Add GeoJson layer.
+  L.geoJson(data, {
+    // Turn feautres into circleMarker
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    // Determine style for each circleMarker and create popup display
+    // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(map);
+
+  // Create legend control object.
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+  // Add info to legend
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+    // Loop through to generate labels and add legend to map
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  legend.addTo(map);
+});
